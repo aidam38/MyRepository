73264dc45beb401d28e879e6dd01f1a723d40f47
Adam <Adam@DESKTOP-21FEV8D>
2017-03-18 10:20:55 +0100

    a

--- PlayField/src/YouCanPlayHere/devataUloha.java
+++ PlayField/src/YouCanPlayHere/devataUloha.java
@@ -1,14 +1,20 @@
 package YouCanPlayHere;
 
 import java.util.Scanner;
+import static java.lang.Math.*;
 
 public class devataUloha {
 	static Scanner scn = new Scanner(System.in);
+	static double x, y;
 
 	public static void main(String[] args) {
-		float x = Float.parseFloat(scn.next());
-		float y = Float.parseFloat(scn.next());
-		System.out.println((x - y) / (1 + x * y));
+		// x = Float.parseFloat(scn.next());
+		// y = Float.parseFloat(scn.next());
+		x = sqrt(6);
+		y = 1.02062;
+		double number = (x - y) / (1 + x * y);
+		double number2 = (sqrt(6) / 6);
+		System.out.println(number + "     " + (number < number2));
 	}
 
 }
